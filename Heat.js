e54cb02c0bb17a6ae60806bda49dc06c2cada564
Tabitha <williamstabitha1120@gmail.com>
2024-03-24 11:27:12 -0400

    Switch on , Switch off

---
+++ Heat.js
@@ -0,0 +1,28 @@
+function RHINESTONE(){
+
+document.getElementById("polarbear").style.backgroundColor="Hotpink"
+}
+function RENIF(){
+
+document.getElementById("polarbear").style.backgroundColor="Blue"
+}
+function REIP() {
+    
+document.getElementById("polarbear").style.backgroundColor="Yellow"  
+}
+function RIP(){
+
+document.getElementById("polarbear").style.backgroundColor="Green"
+}
+function RUOLOCEGNAHC(){
+
+document.getElementById("polarbear").style.backgroundColor="Red"
+}
+function RIB(){
+
+document.getElementById("polarbear").style.backgroundColor="Orangered"
+}
+function FUZZ(){
+
+document.getElementById("polarbear").style.backgroundColor="Black"
+}
